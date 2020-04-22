adb80bd102156442154f8f2c9a9e6034c76869ea
Diego Pinochet <dppinochet@miuandes.cl>
2020-04-22 11:24:19 -0400

    primer commit con .gitignore

---
+++ Pinochet-Diego-Lab5POO/Pinochet-Diego-Lab5POO/ChangePasswordEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinochet_Diego_Lab5POO
+{
+    public class ChangePasswordEventArgs
+    {
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Username { get; set; }
+    }
+}
